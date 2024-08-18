00f2f46888ab7cfaac9194d7258573d9eff4918f
wilkofm <maxferriswilkinson@gmail.com>
2024-08-18 22:31:16 +1000

    Updating rolling script.js to include throw errors

--- script.js
+++ script.js
@@ -1,11 +1,23 @@
 document.getElementById("rollButton").addEventListener("click", function () {
-  const diceType = document.getElementById("diceType").value;
-  let max = 6;
-  if (diceType === "D10") {
-    max = 10;
+  try {
+    const diceType = document.getElementById("diceType").value;
+    let max = 6;
+    if (diceType === "D10") {
+      max = 10;
+    }
+    const randomNumber = Math.floor(Math.random() * max) + 1;
+    document.getElementById(
+      "result"
+    ).textContent = `You rolled ${randomNumber}!`;
+
+    if (randomNumber > 10) {
+      throw new Error("This is incorrect");
+    }
+
+    if (randomNumber < 1) {
+      throw new Error("This is wrong");
+    }
+  } catch (e) {
+    document.getElementById("result").textContent = `Error: ${e.message}`;
   }
-  const randomNumber = Math.floor(Math.random() * max) + 1;
-  document.getElementById(
-    "result"
-  ).textContent = `You rolled ${randomNumber}!`;
 });
